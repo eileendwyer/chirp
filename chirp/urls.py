b8ead96d7a95faeaa12b533874d2b4784c5eee60
Eileen Dwyer <eolson@smalltalku.com>
2016-06-20 12:33:01 -0400

    user auth started

--- chirp/urls.py
+++ chirp/urls.py
@@ -1,9 +1,10 @@
 
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from main.views import IndexView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', IndexView.as_view(), name="index_view"),
+    url('^', include('django.contrib.auth.urls')),
 ]
