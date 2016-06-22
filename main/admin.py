23934469ea5894d385e892baaec6eff457d087bf
Eileen Dwyer <eolson@smalltalku.com>
2016-06-22 10:33:02 -0400

    added form validation

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from main.models import Chirp
+from main.models import Chirp, StopWord
 
 # Register your models here.
 class ChirpAdmin(admin.ModelAdmin):
@@ -7,3 +7,9 @@ class ChirpAdmin(admin.ModelAdmin):
     search_fields = ["body"]
 
 admin.site.register(Chirp, ChirpAdmin)
+
+class StopWordAdmin(admin.ModelAdmin):
+    list_display = ["word"]
+    search_fields = ["word"]
+# list display negates need for __str__
+admin.site.register(StopWord, StopWordAdmin)
--- main/views.py
+++ main/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
-from main.models import Chirp
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView
+from main.models import Chirp, StopWord
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = "index.html"
     model = Chirp
 
@@ -10,3 +11,30 @@ class IndexView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["amount"] = Chirp.objects.all().count()
         return context
+
+class ChirpDetailView(DetailView):
+    model = Chirp
+
+    def get_queryset(self):
+        return Chirp.objects.filter(bird=self.request.user)
+
+class ChirpCreateView(CreateView):
+    model = Chirp
+    fields = ["body"]
+    success_url = "/"
+
+    def form_valid(self, form):
+        # if trump/clinton/or sanders in body, add error
+        #stop_words = ["trump", "sanders", "clinton"] - don't hardcode - do below
+        stop_words = StopWord.objects.all()
+        chirp_body = form.cleaned_data["body"].lower()
+        for stop_word in stop_words:
+        #raise Exception(chirp_body)
+            if stop_word.word in chirp_body:
+                form.add_error("body", "POLITICAL DISCUSSION REVOKED!")
+            return self.form_invalid(form)
+            raise Exception("POLITICAL DISCUSSION REVOKED!")
+
+        chirp = form.save(commit=False)
+        chirp.bird = self.request.user
+        return super().form_valid(form)
